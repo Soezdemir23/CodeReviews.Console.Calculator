1c6beccc1a9b29b007bad2499f38909805f60d21
Soezdemir23 <soezdemir3423@gmail.com>
2026-02-08 18:03:01 +0000

    feat: add CalculatorHistory class for tracking calculation operations

---
+++ CalculatorLibrary.Soezdemir23/CalculationHistory.cs
@@ -0,0 +1,27 @@
+namespace CalculatorLibrary;
+
+public class CalculatorHistory
+{
+    private string operation;
+    private double firstNum;
+    private double secondNum;
+    private double result;
+    private DateTime date;
+
+    public string getOperation() => operation;
+    public void setOperation(string operation) => this.operation = operation;
+
+    public double getFirstNum() => firstNum;
+    public void setFirstNum(double firstNum) => this.firstNum = firstNum;
+
+    public double getSecondNum() => secondNum;
+    public void setSecondNum(double secondNum) => this.secondNum = secondNum;
+
+    public double getResult() => result;
+    public void setResult(double result) => this.result = result;
+
+    public DateTime getDate() => date;
+    public void setDate(DateTime date) => this.date = date;
+
+
+}
