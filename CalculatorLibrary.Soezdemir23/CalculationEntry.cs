5b55ab53a46f2f464c23880452198c6c01dcbfbc
Soezdemir23 <soezdemir3423@gmail.com>
2026-02-15 07:47:52 +0000

    refactor: simplify CalculationEntry properties and remove unnecessary getters/setters

--- CalculatorLibrary.Soezdemir23/CalculationEntry.cs
+++ CalculatorLibrary.Soezdemir23/CalculationEntry.cs
@@ -3,37 +3,23 @@ namespace CalculatorLibrary;
 public class CalculationEntry
 {
 
-    private string operation;
-    private double firstNum;
-    private double secondNum;
-    private double result;
-    private DateTime date;
+
+    public string Operation { get; set; }
+    public double FirstNum { get; set; }
+    public double SecondNum { get; set; }
+    public double Result { get; set; }
+    public DateTime Date { get; set; }
 
     public CalculationEntry(
         string operation, double firstNum,
         double secondNum, double result, DateTime date)
     {
-        this.operation = operation;
-        this.firstNum = firstNum;
-        this.secondNum = secondNum;
-        this.result = result;
-        this.date = date;
+        Operation = operation;
+        FirstNum = firstNum;
+        SecondNum = secondNum;
+        Result = result;
+        Date = date;
 
     }
 
-
-    public string getOperation() => operation;
-    public void setOperation(string operation) => this.operation = operation;
-
-    public double getFirstNum() => firstNum;
-    public void setFirstNum(double firstNum) => this.firstNum = firstNum;
-
-    public double getSecondNum() => secondNum;
-    public void setSecondNum(double secondNum) => this.secondNum = secondNum;
-
-    public double getResult() => result;
-    public void setResult(double result) => this.result = result;
-
-    public DateTime getDate() => date;
-    public void setDate(DateTime date) => this.date = date;
 }
